46cc124ce7d4e4ad2047d9449545cde9679c8325
杨晓凯 <yangxiaokai@topnet.net.cn>
2026-01-01 21:27:13 +0800

    bug fix: 优化拦截器抛出异常让调用方自行处理

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,13 +1,14 @@
-import { showError } from './utils';
-import axios from 'axios';
+import { showError } from "./utils";
+import axios from "axios";
 
 export const API = axios.create({
-  baseURL: process.env.REACT_APP_SERVER ? process.env.REACT_APP_SERVER : '',
+  baseURL: process.env.REACT_APP_SERVER ? process.env.REACT_APP_SERVER : "",
 });
 
 API.interceptors.response.use(
   (response) => response,
   (error) => {
-    showError(error);
+    // 不在拦截器中显示错误，让调用方自行处理
+    return Promise.reject(error);
   }
 );
